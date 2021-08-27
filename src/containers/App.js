2f50c7134a461f9c2ac53c485d42cf55560a497d
Abhishek Chandrasenan <abhishekc@sawolabs.com>
2021-08-27 21:28:29 +0530

    Implemented cookie sessions in the project.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,26 +1,33 @@
-import { useEffect, useState } from 'react';
-import './App.css';
-import Dashboard from './dashboard/Dashboard';
-import SawoLogin from './sawo-login/SawoLogin';
+import { useEffect, useState } from "react";
+import "./App.css";
+import Dashboard from "./dashboard/Dashboard";
+import SawoLogin from "./sawo-login/SawoLogin";
+
+import { useCookies } from "react-cookie";
 
 function App() {
-	const [loginStatus, setLoginStatus] = useState(false);
-	const user = localStorage.getItem('user_id');
-	useEffect(() => {
-		const userId = localStorage.getItem('user_id');
-		if (userId) {
-			setLoginStatus(true);
-		}
-		//write sawo api to compare between local storage userid
-		//and sawo payload
-	}, [loginStatus]);
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [cookies, setCookie, removeCookie] = useCookies(["payload"]);
+  const user = localStorage.getItem("user_id");
 
-	return (
-		<div className='App'>
-			{!loginStatus && <SawoLogin setLoginStatus={setLoginStatus} />}
-			{loginStatus && <Dashboard userId={user} />}
-		</div>
-	);
+  useEffect(() => {
+    if (cookies.payload) {
+      setLoginStatus(true);
+    } else {
+      setLoginStatus(false);
+    }
+    //write sawo api to compare between local storage userid
+    //and sawo payload
+  }, [loginStatus, cookies]);
+
+  return (
+    <div className="App">
+      {!loginStatus && (
+        <SawoLogin setLoginStatus={setLoginStatus} setCookie={setCookie} />
+      )}
+      {loginStatus && <Dashboard userId={user} removeCookie={removeCookie} />}
+    </div>
+  );
 }
 
 export default App;
--- src/containers/dashboard/Dashboard.js
+++ src/containers/dashboard/Dashboard.js
@@ -1,26 +1,34 @@
-import React, { useState } from 'react';
-import IpModal from '../../components/IpModal/IpModal';
-import SearchBar from '../../components/SearchBar/SearchBar';
-import Map from '../../components/Map/Map';
-import '../../styles/typography/typography.css';
-import './Dashboard.css';
+import React, { useState } from "react";
+import IpModal from "../../components/IpModal/IpModal";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Map from "../../components/Map/Map";
+import "../../styles/typography/typography.css";
+import "./Dashboard.css";
+import Logout from "../../components/Logout/Logout";
 
 function Dashboard(props) {
-	const [searchedIp, setSearchedIp] = useState('');
-	const [ip, setIp] = useState(null);
+  const [searchedIp, setSearchedIp] = useState("");
+  const [ip, setIp] = useState(null);
 
-	return (
-		<div>
-			<div className='heading-search'>
-				<div className='main-section'>
-					<h1 className='heading rubik-700-white'>IP Address Tracker</h1>
-					<SearchBar setSearchedIp={setSearchedIp} />
-					<IpModal searchedIp={searchedIp} ip={ip} setIp={setIp} userId={props.userId} />
-				</div>
-			</div>
-			{ip && <Map ip={ip}></Map>}
-		</div>
-	);
+  return (
+    <div>
+      <div className="heading-search">
+        <div className="main-section">
+          <Logout removeCookie={props.removeCookie} />
+
+          <h1 className="heading rubik-700-white">IP Address Tracker</h1>
+          <SearchBar setSearchedIp={setSearchedIp} />
+          <IpModal
+            searchedIp={searchedIp}
+            ip={ip}
+            setIp={setIp}
+            userId={props.userId}
+          />
+        </div>
+      </div>
+      {ip && <Map ip={ip}></Map>}
+    </div>
+  );
 }
 
 export default Dashboard;
